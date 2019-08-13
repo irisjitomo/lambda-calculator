ac1c8c42b64623c47f3c27b4af98380fe44036bb
irisjitomo <irisjitomo@eyahoo.com>
2019-08-13 18:55:05 -0400

    styled calculator

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -5,7 +5,7 @@ const SpecialButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button>{props.text}</button>
+      <button className="special-button">{props.text}</button>
     </>
   );
 };
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return <div className='display'>
+    <div className="display-value">0</div>
+  </div>;
 };
+
+export default Display;
